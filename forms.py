e22d97850ef38c1c9dd604acae216c14fce100cd
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-11-20 14:17:43 +0000

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 class AddNewItemForm(FlaskForm):
     name = StringField("New Item Name", validators=[DataRequired()])
     add_item = SubmitField("Add New Item")
+class LoginForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    login_in = SubmitField("Log me in!")
+
+class TakeItem(FlaskForm):
+    take_item = BooleanField("take")
+
+class EditForm(FlaskForm):
+    edit = SubmitField()
+
+
