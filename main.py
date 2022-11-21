3191fae13b0bbdbc179512514ee279e141803735
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-11-21 10:51:55 +0000

    procfile

--- main.py
+++ main.py
@@ -122,5 +122,5 @@ def take_items(item_id):
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
